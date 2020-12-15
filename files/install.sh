bd478ba45dcfc2c83383875ae7130b6e06a8e450
jadiunr <jadiunr@gmail.com>
2020-12-16 07:50:04 +0900

    WIP

--- files/install.sh
+++ files/install.sh
@@ -5,7 +5,7 @@
 TYPE=$1
 NAME=boundary
 
-sudo cat << EOF > /etc/systemd/system/${NAME}-${TYPE}.service
+cat << EOF > /etc/systemd/system/${NAME}-${TYPE}.service
 [Unit]
 Description=${NAME} ${TYPE}
 
@@ -23,17 +23,17 @@ EOF
 
 # Add the boundary system user and group to ensure we have a no-login
 # user capable of owning and running Boundary
-sudo adduser --system --group boundary || true
-sudo chown boundary:boundary /etc/${NAME}-${TYPE}.hcl
+adduser --system --group boundary || true
+chown boundary:boundary /etc/${NAME}-${TYPE}.hcl
 
 # Make sure to initialize the DB before starting the service. This will result in
 # a database already initizalized warning if another controller or worker has done this
 # already, making it a lazy, best effort initialization
 if [ "${TYPE}" = "controller" ]; then
-  sudo /usr/bin/boundary database init -config /etc/${NAME}-${TYPE}.hcl || true
+  /usr/bin/boundary database init -config /etc/${NAME}-${TYPE}.hcl > /vagrant/init.txt 2>&1 || true
 fi
 
-sudo chmod 664 /etc/systemd/system/${NAME}-${TYPE}.service
-sudo systemctl daemon-reload
-sudo systemctl enable ${NAME}-${TYPE}
-sudo systemctl start ${NAME}-${TYPE}
+chmod 664 /etc/systemd/system/${NAME}-${TYPE}.service
+systemctl daemon-reload
+systemctl enable ${NAME}-${TYPE}
+systemctl start ${NAME}-${TYPE}

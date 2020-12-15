35a85bbeb7570562e4709c3aedff17d98fd7740c
jadiunr <jadiunr@gmail.com>
2020-12-16 07:16:28 +0900

    WIP

--- provision/postgres.sh
+++ provision/postgres.sh
@@ -13,5 +13,6 @@ echo "host all all all md5" >> /etc/postgresql/13/main/pg_hba.conf
 sed -ri "s!^#?(listen_addresses)\s*=\s*\S+.*!\1 = '*'!" /etc/postgresql/13/main/postgresql.conf
 
 systemctl restart postgresql
-sudo -u postgres -i psql -c "create user boundary with password 'boundary'"
-sudo -u postgres -i psql -c "create database 'boundary'"
+sudo -u postgres -i psql -c "create role boundary with login password 'boundary'"
+sudo -u postgres -i psql -c "create database boundary"
+sudo -u postgres -i psql -c "grant all privileges on database boundary to boundary"
